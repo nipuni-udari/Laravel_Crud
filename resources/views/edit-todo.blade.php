3ecc53af324118f3a7bbb0c8c14296e47c80326b
nipuni-udari <96770@fhss.sjp.ac.lk>
2023-08-16 09:01:01 +0530

    chnage the welcome page

--- resources/views/edit-todo.blade.php
+++ resources/views/edit-todo.blade.php
@@ -8,9 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Todo App</title>
 </head>
 <body>
+    <div class="container">
 <div class="text-center mt-5">
     <h2>Edit Todo</h2>
 </div>
@@ -37,6 +38,7 @@
 
             </div>
         </div>
+    </div>
 </form>
 </body>
 </html>
--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -12,7 +12,7 @@
     <title>Todo App</title>
 </head>
 <body>
-
+<div class="container">
     <div class="row justify-content-center mt-5">
         <div class="col-lg-6">
             @if(session()->has('success'))
@@ -96,6 +96,7 @@
               </table>
         </div>
     </div>
+</div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
